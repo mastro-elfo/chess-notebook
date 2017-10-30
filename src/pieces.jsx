0a8f3a0d9dc998dcc059095204995d6ed728814f
mastro-elfo <francesco.209@gmail.com>
2017-10-31 00:09:45 +0100

    Add pieces.jsx

---
+++ src/pieces.jsx
@@ -0,0 +1,21 @@
+import kingWhite from './pieces/K.svg';
+import queenWhite from './pieces/Q.svg';
+import rookWhite from './pieces/R.svg';
+import knightWhite from './pieces/N.svg';
+import bishopWhite from './pieces/B.svg';
+import pawnWhite from './pieces/P.svg';
+import kingBlack from './pieces/k.svg';
+import queenBlack from './pieces/q.svg';
+import rookBlack from './pieces/r.svg';
+import knightBlack from './pieces/n.svg';
+import bishopBlack from './pieces/b.svg';
+import pawnBlack from './pieces/p.svg';
+
+export const PIECES = {
+	'K': kingWhite, 'k': kingBlack,
+	'Q': queenWhite, 'q': queenBlack,
+	'R': rookWhite, 'r': rookBlack,
+	'N': knightWhite, 'n': knightBlack,
+	'B': bishopWhite, 'b': bishopBlack,
+	'P': pawnWhite, 'p': pawnBlack
+};
