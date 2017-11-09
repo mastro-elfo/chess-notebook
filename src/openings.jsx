9326829c2c1a33596029e2da7aa20510a50fae9c
mastro-elfo <francesco.209@gmail.com>
2017-11-09 17:45:08 +0100

    Added some openings

--- src/openings.jsx
+++ src/openings.jsx
@@ -1,5 +1,70 @@
 export const OPENINGS = [
 	{
+		name: "Anderssen opening",
+		fen: 'rnbqkbnr/pppppppp/8/8/8/P7/1PPPPPPP/RNBQKBNR b KQkq - 0 1',
+		eco: 'A00'
+	},
+	{
+		name: "Saragossa opening",
+		fen: 'rnbqkbnr/pppppppp/8/8/8/2P5/PP1PPPPP/RNBQKBNR b KQkq - 0 1',
+		eco: 'A00'
+	},
+	{
+		name: "Mieses opening",
+		fen: 'rnbqkbnr/pppppppp/8/8/8/3P4/PPP1PPPP/RNBQKBNR b KQkq - 0 1',
+		eco: 'A00'
+	},
+	{
+		name: "Gedult's opening",
+		fen: 'rnbqkbnr/pppppppp/8/8/8/5P2/PPPPP1PP/RNBQKBNR b KQkq - 0 1',
+		eco: 'A00'
+	},
+	{
+		name: "Hungarian opening",
+		fen: 'rnbqkbnr/pppppppp/8/8/8/6P1/PPPPPP1P/RNBQKBNR b KQkq - 0 1',
+		eco: 'A00'
+	},
+	{
+		name: "Clemenz opening",
+		fen: 'rnbqkbnr/pppppppp/8/8/8/7P/PPPPPPP1/RNBQKBNR b KQkq - 0 1',
+		eco: 'A00'
+	},
+	{
+		name: "Grob opening",
+		fen: 'rnbqkbnr/pppppppp/8/8/6P1/8/PPPPPP1P/RNBQKBNR b KQkq - 0 1',
+		eco: 'A00'
+	},
+	{
+		name: "Polish opening",
+		fen: 'rnbqkbnr/pppppppp/8/8/1P6/8/P1PPPPPP/RNBQKBNR b KQkq - 0 1',
+		eco: 'A00'
+	},
+	{
+		name: "Sodium attack",
+		fen: 'rnbqkbnr/pppppppp/8/8/8/N7/PPPPPPPP/R1BQKBNR b KQkq - 1 1',
+		eco: 'A00'
+	},
+	{
+		name: "Van Greet opening",
+		fen: 'rnbqkbnr/pppppppp/8/8/8/2N5/PPPPPPPP/R1BQKBNR b KQkq - 1 1',
+		eco: 'A00'
+	},
+	{
+		name: "Amar opening",
+		fen: 'rnbqkbnr/pppppppp/8/8/8/7N/PPPPPPPP/RNBQKB1R b KQkq - 1 1',
+		eco: 'A00'
+	},
+	{
+		name: "Van't Kruijs opening",
+		fen: 'rnbqkbnr/pppppppp/8/8/8/4P3/PPPP1PPP/RNBQKBNR b KQkq - 0 1',
+		eco: 'A00'
+	},
+	{
+		name: "Nimzo-Larsen attack",
+		fen: 'rnbqkbnr/pppppppp/8/8/8/1P6/P1PPPPPP/RNBQKBNR b KQkq - 0 1',
+		eco: 'A01'
+	},
+	{
 		name: "Queen's Pawn",
 		fen: 'rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq d3 0 1'
 	},
