545dbfc330982ca075c9756d9b7342466b4a4ab8
mastro-elfo <francesco.209@gmail.com>
2017-11-10 17:22:41 +0100

    Add openings to A39

--- src/openings.jsx
+++ src/openings.jsx
@@ -125,11 +125,43 @@ export const OPENINGS = [
 		eco: 'A09'
 	},
 	{
+		name: "English opening",
+		fen: 'rnbqkbnr/pppppppp/8/8/2P5/8/PP1PPPPP/RNBQKBNR b KQkq - 0 1',
+		eco: 'A10'
+	},
+	{
+		name: "English opening: Caro-Kann defensive system",
+		fen: 'rnbqkbnr/pp1ppppp/2p5/8/2P5/8/PP1PPPPP/RNBQKBNR w KQkq - 0 2',
+		eco: 'A11'
+	},
+	{
+		name: "English opening: Caro-Kann defensive system",
+		fen: 'rnbqkbnr/pp2pppp/2p5/3p4/2P5/1P3N2/P2PPPPP/RNBQKB1R b KQkq – 0 3',
+		eco: 'A12'
+	},
+	{
+		name: "English opening: Agincourt defense",
+		fen: 'rnbqkbnr/pppp1ppp/4p3/8/2P5/8/PP1PPPPP/RNBQKBNR w KQkq - 0 2',
+		eco: 'A13'
+	},
+	{
+		name: "English Opening: Agincourt defense, Neo-Catalan declined",
+		fen: 'rnbqk2r/ppp1bppp/4pn2/3p4/2P5/5NP1/PP1PPPBP/RNBQ1RK1 b kq - 4 5',
+		eco: 'A14'
+	},
+	{
+		name: "English Opening: Anglo-Indian Defense",
+		fen: 'rnbqkb1r/pppppppp/5n2/8/2P5/8/PP1PPPPP/RNBQKBNR w KQkq - 1 2',
+		eco: 'A15'
+	},{"name":"English Opening: Anglo-Indian Defense","fen":"rnbqkb1r/pppppppp/5n2/8/2P5/8/PP1PPPPP/RNBQKBNR w KQkq - 1 2","eco":"A15"},{"name":"English Opening: Anglo-Indian Defense, Queen's Knight Variation","fen":"rnbqkb1r/pppppppp/5n2/8/2P5/2N5/PP1PPPPP/R1BQKBNR b KQkq - 2 2","eco":"A16"},{"name":"English Opening: Anglo-Indian Defense, Hedgehog System","fen":"rnbqkb1r/pppp1ppp/4pn2/8/2P5/2N5/PP1PPPPP/R1BQKBNR w KQkq - 0 3","eco":"A17"},{"name":"English Opening: Mikenas-Carls Variation","fen":"rnbqkb1r/ppp2ppp/4pn2/3pP3/2P5/2N5/PP1P1PPP/R1BQKBNR b KQkq - 0 4","eco":"A18"},{"name":"English Opening: Mikenas-Carls, Sicilian","fen":"rnbqkb1r/pp1p1ppp/4pn2/2p5/2P1P3/2N5/PP1P1PPP/R1BQKBNR w KQkq c6 0 4","eco":"A19"},{"name":"English Opening: King's English Variation, Two Knights Variation","fen":"rnbqkb1r/pppp1ppp/5n2/4p3/2P5/2N5/PP1PPPPP/R1BQKBNR w KQkq - 2 3","eco":"A22"},{"name":"English Opening: King's English Variation, Two Knights Variation","fen":"rnbqkb1r/pppp1ppp/5n2/4p3/2P5/2N5/PP1PPPPP/R1BQKBNR w KQkq - 2 3","eco":"A22"},{"name":"English Opening: King's English Variation, Two Knights Variation, Keres Variation","fen":"rnbqkb1r/pp1p1ppp/2p2n2/4p3/2P5/2N3P1/PP1PPP1P/R1BQKBNR w KQkq - 0 4","eco":"A23"},{"name":"English Opening: King's English Variation, Two Knights Variation, Fianchetto Line","fen":"rnbqkb1r/pppp1p1p/5np1/4p3/2P5/2N3P1/PP1PPP1P/R1BQKBNR w KQkq - 0 4","eco":"A24"},{"name":"English Opening: King's English Variation, Reversed Closed Sicilian","fen":"r1bqkbnr/pppp1ppp/2n5/4p3/2P5/2N5/PP1PPPPP/R1BQKBNR w KQkq - 2 3","eco":"A25"},{"name":"English Opening: King's English Variation, Closed System, Full Symmetry","fen":"r1bqk1nr/ppp2pbp/2np2p1/4p3/2P5/2NP2P1/PP2PPBP/R1BQK1NR w KQkq - 0 6","eco":"A26"},{"name":"English Opening: King's English Variation, Three Knights System","fen":"r1bqkbnr/pppp1ppp/2n5/4p3/2P5/2N2N2/PP1PPPPP/R1BQKB1R b KQkq - 3 3","eco":"A27"},{"name":"English Opening: King's English Variation, Four Knights Variation #2","fen":"r1bqkb1r/pppp1ppp/2n2n2/4p3/2P5/2N2N2/PP1PPPPP/R1BQKB1R w KQkq - 4 4","eco":"A28"},{"name":"English Opening: King's English Variation, Four Knights Variation, Fianchetto Line","fen":"r1bqkb1r/pppp1ppp/2n2n2/4p3/2P5/2N2NP1/PP1PPP1P/R1BQKB1R b KQkq - 0 4","eco":"A29"},{"name":"English Opening: Symmetrical Variation, Anti-Benoni Variation","fen":"rnbqkb1r/pp1ppppp/5n2/2p5/2PP4/5N2/PP2PPPP/RNBQKB1R b KQkq d3 0 3","eco":"A31"},{"name":"English Opening: Symmetrical Variation, Anti-Benoni Variation, Spielmann Defense","fen":"rnbqkb1r/pp1p1ppp/4pn2/8/2PN4/8/PP2PPPP/RNBQKB1R w KQkq - 0 5","eco":"A32"},{"name":"English Opening: Symmetrical Variation, Anti-Benoni Variation, Spielmann Defense","fen":"r1bqkb1r/pp1p1ppp/2n1pn2/8/2PN4/2N5/PP2PPPP/R1BQKB1R w KQkq - 2 6","eco":"A33"},{"fen":"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"},{"name":"English Opening: Symmetrical Variation, Two Knights Variation","fen":"r1bqkbnr/pp1ppppp/2n5/2p5/2P5/2N5/PP1PPPPP/R1BQKBNR w KQkq - 2 3","eco":"A35"},{"name":"English Opening: Symmetrical Variation, Fianchetto Variation","fen":"r1bqkbnr/pp1ppppp/2n5/2p5/2P5/2N3P1/PP1PPP1P/R1BQKBNR b KQkq - 0 3","eco":"A36"},{"name":"English Opening: Symmetrical Variation, Two Knights Line","fen":"r1bqk1nr/pp1pppbp/2n3p1/2p5/2P5/2N2NP1/PP1PPPBP/R1BQK2R b KQkq - 3 5","eco":"A37"},{"name":"English Opening: Symmetrical Variation, Full Symmetry Line","fen":"r1bqk2r/pp1pppbp/2n2np1/2p5/2P5/2N2NP1/PP1PPPBP/R1BQK2R w KQkq - 4 6","eco":"A38"},{"name":"English Opening: Symmetrical Variation, Mecking Variation","fen":"r1bq1rk1/pp1pppbp/2n2np1/2p5/2PP4/2N2NP1/PP2PPBP/R1BQ1RK1 b - d3 0 7","eco":"A39"},
+	{
 		name: "Queen's Pawn",
-		fen: 'rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq d3 0 1'
+		fen: 'rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq d3 0 1',
+		eco: ''
 	},
 	{
 		name: "King's Pawn",
-		fen: 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1'
+		fen: 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1',
+		eco: ''
 	}
 ];
