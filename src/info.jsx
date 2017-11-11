ffc179871b177eb0581a9fd874ecf4d711e9f181
mastro-elfo <francesco.209@gmail.com>
2017-11-11 16:06:37 +0100

    More info added

--- src/info.jsx
+++ src/info.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {ICONS} from './icons';
 import {LinkButton} from './Button';
+import {OPENINGS} from './openings';
 
 export default class Info extends React.Component {
 	render(){
@@ -51,6 +52,24 @@ OTHER DEALINGS IN THE SOFTWARE.
 							<li>
 								<h3>Chess pieces</h3>
 								<p>By jurgenwesterhof (adapted from work of Cburnett) - <a className="external free" href="http://commons.wikimedia.org/wiki/Template:SVG_chess_pieces">http://commons.wikimedia.org/wiki/Template:SVG_chess_pieces</a>, <a href="https://creativecommons.org/licenses/by-sa/3.0" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=35634436">Link</a></p>
+							</li>
+						</ul>
+
+						<h2>Development</h2>
+						<ul className="list">
+							<li>
+								<h3>Repository on Github</h3>
+								<p>
+									<a className="external" title="Click to open Chess-Notebook repository on Github" href="https://github.com/mastro-elfo/chess-notebook">https://github.com/mastro-elfo/chess-notebook</a>
+								</p>
+							</li>
+						</ul>
+
+						<h2>Chess-Notebook</h2>
+						<ul className="list">
+							<li>
+								<h3>Openings</h3>
+								<p>There are {OPENINGS.length} openings in the library</p>
 							</li>
 						</ul>
 					</div>
