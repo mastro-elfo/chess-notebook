b660745a310905da0075ec41ef137259e93052bf
mastro-elfo <francesco.209@gmail.com>
2018-09-10 18:02:49 +0200

    Start migration

--- src/newgame.jsx
+++ src/newgame.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import Chess from 'chess.js/chess.min.js';
-import Chessboard, {Pool} from './chessboard';
-import {GameStorage} from './storage';
-import Modal, {ModalButton, ModalButtons} from './modal';
-import {ICONS} from './icons';
-import './newgame.css';
-import {Button} from './Button';
+import React, {Component} from 'react';
 
-export default class New extends React.Component {
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+
+import Chess from 'chess.js/chess.min.js';
+
+import Chessboard, {Pool} from './chessboard';
+import {GameStorage, LineStorage, SettingsStorage} from './storage';
+
+export default class New extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
@@ -32,7 +43,7 @@ export default class New extends React.Component {
 	componentDidMount(){
 		window.addEventListener('resize', this.onResize.bind(this));
 		this.onResize();
-		this.refs.title.focus();
+		// this.refs.title.focus();
 	}
 
 	onResize(){
@@ -215,192 +226,91 @@ export default class New extends React.Component {
 		const enPassantCandidates = this.getEnPassantCandidates();
 		const selectableCells = this.getSelectableCells();
 		return (
-			<section className="Newgame">
-				<header>
-					<div>
-						<Button to="/" className="left" title="Go back" onClick={this.props.history.goBack}>
-							<img alt="back" src={ICONS['back']}/>
+			<div>
+				<AppBar position="static">
+					<Toolbar>
+						<IconButton
+							onClick={()=>this.props.history.goBack()}>
+							<ArrowBackIcon/>
+						</IconButton>
+						<Typography variant="title" style={{flexGrow: 1}}>
+							Create new Game
+						</Typography>
+
+						<Button>
+							PGN
 						</Button>
-						<Button className="right" onClick={this.onClickPlayGame.bind(this)} title="Play this position">
-							<img alt="Play" src={ICONS['play']}/>
+
+						<Button>
+							Reset
 						</Button>
-						<Button className="right" disabled={this.state.position === 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'} onClick={this.onClickResetPosition.bind(this)} title="Reset to start position">
-							<img alt="Reset" src={ICONS['rew']}/>
-						</Button>
-						<Button className="right" title="Load from PGN" onClick={this.onClickOpenFromPGNDialog.bind(this)}>
-							<img alt="pgn" src={ICONS['pgn']}/>
-						</Button>
-						<h1>New</h1>
-					</div>
-				</header>
-				<main>
-					<div>
-						<ul className="list">
-							<li>
-								<label>
-									<h3>Title</h3>
-									<input ref="title" placeholder="E.g. &quot;against the local champion&quot;" value={this.state.title} onChange={(e)=>this.setState({title: e.target.value})}/>
-								</label>
-							</li>
-						</ul>
-						<div style={{height: this.state.componentSize}}>
-							<div className="column column-4 whitePool">
-								<Pool pieces="KQRNBP" onClick={this.onClickPiece.bind(this)} selected={this.state.selectedPiece}/>
-							</div>
-							<div className="column column-2">
-								<Chessboard fen={this.toFen()} selectableCells={selectableCells} onClick={this.onClickCell.bind(this)} onDrop={this.onDropCell.bind(this)}/>
-							</div>
-							<div className="column column-4 blackPool">
-								<Pool pieces="kqrnbp" onClick={this.onClickPiece.bind(this)} selected={this.state.selectedPiece}/>
-							</div>
-						</div>
-						<div className="clear"></div>
-						<ul className="list">
-							<li>
-								<label>
-									<h3>Description</h3>
-									<textarea
-										placeholder="E.g. &quot;The best match I've ever played&quot;"
-										value={this.state.description}
-										onChange={(e)=>this.setState({description: e.target.value})}
-										></textarea>
-								</label>
-							</li>
-							<li>
-								<label>
-									<h3>Turn</h3>
-									<select
-										value={this.state.turn}
-										onChange={(e)=>this.setState({turn: e.target.value})}
-										>
-										<option value="w">White</option>
-										<option value="b">Black</option>
-									</select>
-								</label>
-							</li>
-							<li>
-								<label>
-									<h3>White castling</h3>
-									<select
-										value={this.state.whiteCastling}
-										onChange={(e)=>this.setState({whiteCastling: e.target.value})}
-										>
-										<option value="KQ">Both sides</option>
-										<option value="K">King side</option>
-										<option value="Q">Queen side</option>
-										<option value="">None</option>
-									</select>
-								</label>
-							</li>
-							<li>
-								<label>
-									<h3>Black castling</h3>
-									<select
-										value={this.state.blackCastling}
-										onChange={(e)=>this.setState({blackCastling: e.target.value})}
-										>
-										<option value="kq">Both sides</option>
-										<option value="k">King side</option>
-										<option value="q">Queen side</option>
-										<option value="">None</option>
-									</select>
-								</label>
-							</li>
-							<li>
-								<label>
-									<h3>En passant</h3>
-									<select
-										value={this.state.enPassant}
-										onChange={(e)=>this.setState({enPassant: e.target.value})}
-										>
-										<option value="-">None</option>
-										{
-											enPassantCandidates.map((cell, i) =>
-												<option key={i} value={cell}>
-													{cell}
-												</option>
-											)
-										}
-									</select>
-								</label>
-							</li>
-							<li>
-								<label>
-									<h3>Draw moves</h3>
-									<input
-										type="number"
-										min="0"
-										max={this.state.totalMoves}
-										placeholder="#"
-										value={this.state.drawMoves}
-										onChange={(e)=>this.setState({drawMoves: e.target.value})}/>
-								</label>
-							</li>
-							<li>
-								<label>
-									<h3>Total moves</h3>
-									<input
-										type="number"
-										min="1"
-										placeholder="#"
-										value={this.state.totalMoves}
-										onChange={(e)=>this.setState({totalMoves: e.target.value})}/>
-								</label>
-							</li>
-							<li>
-								<label>
-									<h3>Fen</h3>
-									<p>
-										{this.toFen()}
-									</p>
-								</label>
-							</li>
-						</ul>
-					</div>
-				</main>
 
-				{this.state.openFromPGNDialog &&
-					<Modal onClose={this.onCancelFromPGNDialog.bind(this)}>
-						<h1>Load from PGN</h1>
-						<p>Paste the PGN in this text input</p>
+						<IconButton
+							onClick={this.onClickPlayGame.bind(this)}>
+							<PlayArrowIcon/>
+						</IconButton>
+					</Toolbar>
+				</AppBar>
+				<FormControl>
+					<TextField
+						label="Title"
+						fullWidth
+						value=""
+						/>
 
-						<textarea placeholder="" value={this.state.pastePGN} onChange={this.onChangePGN.bind(this)}></textarea>
+					<TextField
+						label="Description"
+						fullWidth
+						value=""
+						/>
 
-						{this.state.pastePGNValid === true && <p style={{color: 'green'}}>Valid PGN</p>}
+					{
+						// TODO: maybe insert chessboard here
+					}
 
-						{this.state.pastePGNValid === false && <p style={{color: 'red'}}>Not a valid PGN</p>}
+					<Select>
+						<MenuItem value="">White</MenuItem>
+						<MenuItem value="">Black</MenuItem>
+					</Select>
 
-						{this.state.pastePGNValid !== true && this.state.pastePGNValid !== false && <p>&nbsp;</p>}
+					<Select>
+						<MenuItem value="">Both sides</MenuItem>
+						<MenuItem value="">King side</MenuItem>
+						<MenuItem value="">Queen side</MenuItem>
+						<MenuItem value="">None</MenuItem>
+					</Select>
 
-						<ModalButtons>
-							<ModalButton title="Confirm" disabled={this.state.pastePGNValid !== true} onClick={this.onClickConfirmPGN.bind(this)}>
-								<img src={ICONS['boxChecked']} alt="ok"/> Confirm
-							</ModalButton>
-							<ModalButton title="Cancel" onClick={()=>this.setState({openFromPGNDialog: false})}>
-								<img src={ICONS['delete']} alt="x"/> Cancel
-							</ModalButton>
-						</ModalButtons>
-					</Modal>
-				}
+					<Select>
+						<MenuItem value="">Both sides</MenuItem>
+						<MenuItem value="">King side</MenuItem>
+						<MenuItem value="">Queen side</MenuItem>
+						<MenuItem value="">None</MenuItem>
+					</Select>
 
-				{this.state.requestTitle &&
-					<Modal onClose={()=>this.setState({requestTitle: false})}>
-						<h1>Title required</h1>
-						<p>Title and description are useful to search games.</p>
-						<p>
-							<input placeholder="Write a title" value={this.state.title} onChange={(e)=>this.setState({title: e.target.value})}/>
-						</p>
-						<ModalButtons>
-							<ModalButton title="Confirm" disabled={this.state.title.trim() === ''} onClick={this.onClickPlayGame.bind(this)}>
-								<img src={ICONS['boxChecked']} alt="ok"/> Confirm
-							</ModalButton>
-							<ModalButton title="Cancel" onClick={()=>this.setState({requestTitle: false})}>
-								<img src={ICONS['delete']} alt="x"/> Cancel
-							</ModalButton>
-						</ModalButtons>
-					</Modal>
-				}
-			</section>
+					<Select>
+						<MenuItem value="">None</MenuItem>
+					</Select>
+
+					<TextField
+						label="Draw moves"
+						fullWidth
+						value={0}
+						/>
+
+					<TextField
+						label="Total moves"
+						fullWidth
+						value={1}
+						/>
+
+					<TextField
+						label="Fen"
+						fullWidth
+						readOnly
+						value=""
+						/>
+				</FormControl>
+			</div>
 		);
 	}
 
@@ -432,3 +342,174 @@ export default class New extends React.Component {
 		return output;
 	}
 }
+
+/*
+<main>
+		<div>
+			<ul className="list">
+				<li>
+					<label>
+						<h3>Title</h3>
+						<input ref="title" placeholder="E.g. &quot;against the local champion&quot;" value={this.state.title} onChange={(e)=>this.setState({title: e.target.value})}/>
+					</label>
+				</li>
+			</ul>
+			<div style={{height: this.state.componentSize}}>
+				<div className="column column-4 whitePool">
+					<Pool pieces="KQRNBP" onClick={this.onClickPiece.bind(this)} selected={this.state.selectedPiece}/>
+				</div>
+				<div className="column column-2">
+					<Chessboard fen={this.toFen()} selectableCells={selectableCells} onClick={this.onClickCell.bind(this)} onDrop={this.onDropCell.bind(this)}/>
+				</div>
+				<div className="column column-4 blackPool">
+					<Pool pieces="kqrnbp" onClick={this.onClickPiece.bind(this)} selected={this.state.selectedPiece}/>
+				</div>
+			</div>
+			<div className="clear"></div>
+			<ul className="list">
+				<li>
+					<label>
+						<h3>Description</h3>
+						<textarea
+							placeholder="E.g. &quot;The best match I've ever played&quot;"
+							value={this.state.description}
+							onChange={(e)=>this.setState({description: e.target.value})}
+							></textarea>
+					</label>
+				</li>
+				<li>
+					<label>
+						<h3>Turn</h3>
+						<select
+							value={this.state.turn}
+							onChange={(e)=>this.setState({turn: e.target.value})}
+							>
+							<option value="w">White</option>
+							<option value="b">Black</option>
+						</select>
+					</label>
+				</li>
+				<li>
+					<label>
+						<h3>White castling</h3>
+						<select
+							value={this.state.whiteCastling}
+							onChange={(e)=>this.setState({whiteCastling: e.target.value})}
+							>
+							<option value="KQ">Both sides</option>
+							<option value="K">King side</option>
+							<option value="Q">Queen side</option>
+							<option value="">None</option>
+						</select>
+					</label>
+				</li>
+				<li>
+					<label>
+						<h3>Black castling</h3>
+						<select
+							value={this.state.blackCastling}
+							onChange={(e)=>this.setState({blackCastling: e.target.value})}
+							>
+							<option value="kq">Both sides</option>
+							<option value="k">King side</option>
+							<option value="q">Queen side</option>
+							<option value="">None</option>
+						</select>
+					</label>
+				</li>
+				<li>
+					<label>
+						<h3>En passant</h3>
+						<select
+							value={this.state.enPassant}
+							onChange={(e)=>this.setState({enPassant: e.target.value})}
+							>
+							<option value="-">None</option>
+							{
+								enPassantCandidates.map((cell, i) =>
+									<option key={i} value={cell}>
+										{cell}
+									</option>
+								)
+							}
+						</select>
+					</label>
+				</li>
+				<li>
+					<label>
+						<h3>Draw moves</h3>
+						<input
+							type="number"
+							min="0"
+							max={this.state.totalMoves}
+							placeholder="#"
+							value={this.state.drawMoves}
+							onChange={(e)=>this.setState({drawMoves: e.target.value})}/>
+					</label>
+				</li>
+				<li>
+					<label>
+						<h3>Total moves</h3>
+						<input
+							type="number"
+							min="1"
+							placeholder="#"
+							value={this.state.totalMoves}
+							onChange={(e)=>this.setState({totalMoves: e.target.value})}/>
+					</label>
+				</li>
+				<li>
+					<label>
+						<h3>Fen</h3>
+						<p>
+							{this.toFen()}
+						</p>
+					</label>
+				</li>
+			</ul>
+		</div>
+	</main>
+
+	{this.state.openFromPGNDialog &&
+		<Modal onClose={this.onCancelFromPGNDialog.bind(this)}>
+			<h1>Load from PGN</h1>
+			<p>Paste the PGN in this text input</p>
+
+			<textarea placeholder="" value={this.state.pastePGN} onChange={this.onChangePGN.bind(this)}></textarea>
+
+			{this.state.pastePGNValid === true && <p style={{color: 'green'}}>Valid PGN</p>}
+
+			{this.state.pastePGNValid === false && <p style={{color: 'red'}}>Not a valid PGN</p>}
+
+			{this.state.pastePGNValid !== true && this.state.pastePGNValid !== false && <p>&nbsp;</p>}
+
+			<ModalButtons>
+				<ModalButton title="Confirm" disabled={this.state.pastePGNValid !== true} onClick={this.onClickConfirmPGN.bind(this)}>
+					<img src={ICONS['boxChecked']} alt="ok"/> Confirm
+				</ModalButton>
+				<ModalButton title="Cancel" onClick={()=>this.setState({openFromPGNDialog: false})}>
+					<img src={ICONS['delete']} alt="x"/> Cancel
+				</ModalButton>
+			</ModalButtons>
+		</Modal>
+	}
+
+	{this.state.requestTitle &&
+		<Modal onClose={()=>this.setState({requestTitle: false})}>
+			<h1>Title required</h1>
+			<p>Title and description are useful to search games.</p>
+			<p>
+				<input placeholder="Write a title" value={this.state.title} onChange={(e)=>this.setState({title: e.target.value})}/>
+			</p>
+			<ModalButtons>
+				<ModalButton title="Confirm" disabled={this.state.title.trim() === ''} onClick={this.onClickPlayGame.bind(this)}>
+					<img src={ICONS['boxChecked']} alt="ok"/> Confirm
+				</ModalButton>
+				<ModalButton title="Cancel" onClick={()=>this.setState({requestTitle: false})}>
+					<img src={ICONS['delete']} alt="x"/> Cancel
+				</ModalButton>
+			</ModalButtons>
+		</Modal>
+	}
+</section>
+ */
